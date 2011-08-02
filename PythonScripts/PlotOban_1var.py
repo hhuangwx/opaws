df3e421132a19cea9b15eed5753943a63eff9381
Louis.Wicker <Louis.Wicker@noaa.gov>
2011-08-02 03:20:18 +0000

    New script for single plots of variables - useful when one has WDSS2 input

---
+++ PythonScripts/PlotOban_1var.py
@@ -0,0 +1,335 @@
+#!/usr/bin/env python
+#
+import matplotlib
+import pylab as P
+import numpy as N
+import sys
+import netCDF4
+from optparse import OptionParser
+from netcdftime import utime
+import os
+import markup
+import ctables
+from mpl_toolkits.axes_grid.inset_locator import inset_axes
+from mpl_toolkits.basemap import Basemap
+
+
+output_format = "png"
+
+# ADD the name of the variables you want plotted here.
+#
+ref_variables = ["DBZ", "DZ"]
+#vr_variables  = ["DV", "VE", "VR", "VEL", "VU"]
+
+hscale = 1.0 / 1000.
+
+#===============================================================================
+#
+def dxy_2_ll(x, y, lat1, lon1):
+    """dxy_2_ll returns the approximate lat/lon between an x,y coordinate and
+       a reference lat/lon point.  Assumes a flat earth approximation (which is
+       sufficient for radar data) and should be good out to distances of ~200 km.
+
+       INPUTS:  x,y in meters, lat1, lon1 in radians.  Returns degrees
+
+       if x > 0, lon > lon1
+
+       if y > 0, lat > lat1
+
+       OUTPUTS:  lat, lon in radians
+    """
+    rearth = 1000.0 * 6367.0
+
+    if lon1 < 0.0:
+        lon1p = lon1+2.0*N.pi
+    else:
+        lon1p = lon1
+
+    lat = lat1 + y / rearth
+    lon = lon1 + x / ( rearth * N.cos(0.5*(lat1+lat)) )
+
+    lat = N.rad2deg(lat)
+    lon = N.rad2deg(lon)
+    lon = N.where( lon < 180., lon, 180 - lon)
+
+    return lat, lon
+
+def create_html(directory,files):
+
+    items = tuple(files)
+    paras = ( "Quick and dirty output from oban program" )
+    images = tuple(files)
+    
+    page = markup.page( )
+    page.init( title="My title", 
+    css=( 'one.css' ), 
+    header="Objective Analysis of dBZ from each sweep file") 
+
+# hack in an embedded tag for images - markup.py probably has a way to do this better...
+
+    img_src = []
+    for item in files:
+        img_src.append("<img src='%s',  width='%s', height='%s'>" % (item,"300","300"))
+    
+    page.p( paras )
+    page.a( tuple(img_src), href=images, title=images)
+    
+    html_file = open(str(directory)+".html", "w")
+    html_file.write(page())
+    html_file.close()
+    html_file = open(str(directory)+"/"+str(directory)+".html", "w")
+    html_file.write(page())
+    html_file.close()
+    return
+
+def plotoban(ref, x, y, elevation, az, time, pass_no=1, directory=None):
+
+    filename = "%s/%s_%4.2f_pass_%d.png" % ("./"+directory,str.replace(time.isoformat(),"T","_"), elevation, pass_no)
+               
+#   fig_y_size = 8 * (y.max() - y.min()) / (x.max()-x.min())
+
+    P.figure(figsize=(6,10))
+    P.clf()
+    P.axes().set_aspect('equal')
+
+    plt = P.subplot(111)
+    clevels = N.arange(0,75,5)
+    plt = P.contourf(x, y, ref, clevels, cmap=ctables.NWSRef)
+    cbar = P.colorbar(plt)
+    cbar.ax.set_ylabel('dBZ')
+    P.xlabel('X (km)')
+    P.ylabel('Y (km)')
+    P.ylim(y.min(), y.max())
+    P.xlim(x.min(), x.max())
+    P.title("DBZ Analysis / %s   / %4.2f deg P=%d" % (str.replace(time.isoformat(),"T","_"),elevation, pass_no))
+    
+    P.xlabel('X (km)')
+    P.ylabel('Y (km)')
+
+    if output_format == "png": 
+        print "\n Saving file %s" % (filename)
+        P.savefig(filename, format="png")
+        P.close()
+    else:
+        P.show()
+        P.close()
+
+    return filename
+
+def plotoban_gis(ref, x, y, elevation, az, time, pass_no=1, directory=None, lat=None, lon=None):
+
+    filename = "%s/%s_%4.2f_pass_%d.png" % ("./"+directory,str.replace(time.isoformat(),"T","_"), elevation, pass_no)
+               
+    fig = P.figure(figsize = (10,10))
+    ax  = fig.add_subplot(111)
+
+    xmax = max(x)
+    xmin = min(x)
+    ymax = max(y)
+    ymin = min(y)
+
+    clevels = N.arange(0,75,5)
+    plot    = ax.contourf(x, y, ref, clevels, cmap=ctables.NWSRef)
+    axins   = inset_axes(ax, width="5%", height="35%", loc=3)
+    locator = axins.get_axes_locator()
+    locator.set_bbox_to_anchor((1.05, 0.05, 1, 1), ax.transAxes)
+    locator.borderpad = 0.
+    cbar    = fig.colorbar(plot, cax=axins)
+    ax.set_xlabel('X (km)')
+    ax.set_ylabel('Y (km)')
+    ax.set_title("DBZ Analysis / %s   / %4.2f deg P=%d" % (str.replace(time.isoformat(),"T","_"),elevation, pass_no),ha='right')
+
+    sw_lat, sw_lon = dxy_2_ll(xmin/hscale,ymin/hscale,N.deg2rad(lat),N.deg2rad(lon))
+    ne_lat, ne_lon = dxy_2_ll(xmax/hscale,ymax/hscale,N.deg2rad(lat),N.deg2rad(lon))
+
+# First, set the map up on a grid associated with the lat/lon from the model domain
+
+    map = Basemap(projection='lcc', width=(xmax-xmin)/hscale, height=(ymax-ymin)/hscale, lat_1=sw_lat, lat_2=ne_lat, lat_0=0.5*(ne_lat+sw_lat), lon_0=0.5*(ne_lon+sw_lon), area_thresh=1.,suppress_ticks=True)
+    
+# Set plot limits
+
+    ax.set_aspect('equal')
+
+    ax.set_xlim(xmin, xmax)
+    ax.set_ylim(ymin, ymax)
+
+# Shape file stuff
+
+    try:
+        shapelist = os.getenv("PYESVIEWER_SHAPEFILES").split(":")
+
+        if len(shapelist) > 0:
+
+            mapaxes1 = inset_axes(ax,width="100%",height="100%",loc=3)       
+            mapaxes1.set_axis_off()
+            locator1 = mapaxes1.get_axes_locator()
+            locator1.set_bbox_to_anchor((0,0,1,1),ax.transAxes)
+            locator1.borderpad = 0.0
+
+            for item in shapelist:
+                items = item.split(",")
+                shapefile  = items[0]
+                color      = items[1]
+                line_width = items[2]
+                print shapefile, color, line_width
+
+                map.readshapefile(shapefile,'GIS_INFO',drawbounds=True,linewidth=line_width,color=color,ax=mapaxes1)
+
+            map.readshapefile('/Users/wicker/Downloads/ci01jn11/ci01jn11','GIS_INFO',drawbounds=True,linewidth=line_width,color=color,ax=mapaxes1)
+
+    except OSError:
+           print "GIS_PLOT:  NO SHAPEFILE ENV VARIABLE FOUND " 
+           pass
+
+    if output_format == "png": 
+        print "\n Saving file %s" % (filename)
+        fig.savefig(filename, format="png")
+    else:
+        fig.show()
+
+    return filename
+
+#---------------------------------------------------------------------------------------------------
+# Main function defined to return correct sys.exit() calls
+#
+def main(argv=None):
+    if argv is None:
+           argv = sys.argv
+        
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="file", type="string", default= None, \
+                                      help="Name of netCDF file created from oban analysis")
+    parser.add_option("-d", "--dir",  dest="dir", type="string", \
+                                      help="Directory to create output plots in, default is 'obanplots'")
+    parser.add_option("-s", "--skip", dest="skip", type="int", default=1, \
+                                      help="Skip N sweep files when plotting --> useful for big data sets")
+    parser.add_option("-i", "--interactive", dest="interact", default=False, \
+                                      help = "Boolean flag to show plots interactively default==False", action="store_true")
+    parser.add_option("-g", "--gis",  dest="gis", default=False, \
+                                      help = "Boolean flag to use GIS environment variable default==False", action="store_true")
+
+    (options, args) = parser.parse_args()
+
+    if options.file == None:
+        print
+        parser.print_help()
+        print
+        sys.exit(1)
+    
+    if options.dir == None:
+        options.dir = "obanplots"
+        if not os.path.isdir(options.dir):
+            print "\n%s does not exist, creating directory" % (options.dir)
+            os.mkdir(options.dir)
+    else:
+        if not os.path.isdir(options.dir):
+            print "\n%s does not exist, creating directory" % (options.dir)
+            os.mkdir(options.dir)
+        
+    if not os.path.exists(options.file):
+        print "\nError!  netCDF file does not seem to exist?"
+        print "Filename:  %s" % (options.file)
+        sys.exit(1)
+
+# Decide what backend to use, either PDF (default), or interactive (which uses the users .matplotlib/rcParams file in ~home)
+
+    if options.interact == True:
+        output_format = "i"
+
+    ncdf_file  = netCDF4.Dataset(options.file, "r")
+
+# Here try and read in reflectivity data
+#
+    for item in ref_variables:
+        try:
+            d = ncdf_file.variables[item][:]
+            print "\n ===> Read variable ", item
+        except:
+            print "\n Cannot find variable ", item
+
+    try:
+       print "Shape of reflectivity field:  ", d.shape
+    except UnboundLocalError:
+       print
+       print "**** !!!!ERROR!!!! ****"
+       print
+       print "No valid Reflectivity field found"
+       print "PlotOban looked for the following fields:  ", ref_variables
+       print
+       print "netCDF file has the following variables:  \n" 
+       for item in ncdf_file.variables.keys():  print item 
+       print
+       print "Please add the name of the file's reflectivity field to the list of ref_variables at top of the PlotOban script"
+       print
+       sys.exit(1)
+
+
+# Here read in other data needed for plotting - note failure is not an option!
+#
+    for item in ["EL","AZ","TIME","x","y"]:
+        try:
+            q = ncdf_file.variables[item][:]
+            print "\n ===> Read variable ", item
+        except:
+            print "\n Cannot find %s variable, exiting " % item
+            sys.exit(1)
+
+        if item == "EL":    el = q.copy()
+        if item == "AZ":    az = q.copy()
+        if item == "TIME":  ti = q.copy()
+        if item == "x":     x  = q.copy()
+        if item == "y":     y  = q.copy()
+
+    print x
+    
+
+    del(q)  # releasing some memory
+    
+    date  = netCDF4.chartostring(ncdf_file.variables['start_date'][:])
+    time  = netCDF4.chartostring(ncdf_file.variables['start_time'][:])
+    date2 = str.replace(date.tostring(),"/","-")
+    
+    date_string = "seconds since "+ date2[6:10]+"-"+date2[0:5] + " " + time.tostring()
+    print
+    print "COARDS string from file:  ",date_string
+    
+    sec_utime = utime(date_string)
+
+    var_shape = d.shape
+    
+    print
+    print "Directory images are stored in is:  ", options.dir
+    print "Shape of fields is:  ", var_shape[:]
+    print
+
+    files = []
+    
+    for p in [var_shape[1] - 1]:
+        print "Processing pass = ", p
+        for k in N.arange(0, var_shape[2], options.skip):
+            print "Processing level = ", k, el.shape, ti.shape
+            print "Slice: %3d  Sweep elevation:  %4.2f  Sweep time %s " % \
+                              (k,el[0,k,:,:].mean(), sec_utime.num2date(ti[0,k,:,:].mean()))
+
+            if options.gis:
+              files.append(plotoban_gis(d[0,p,k], x, y, el[0,k].mean(), az[0,k], \
+                           sec_utime.num2date(ti[0,k].mean()), pass_no = p+1, directory = options.dir, \
+                           lat=ncdf_file.variables['grid_latitude'][0], lon=ncdf_file.variables['grid_longitude'][0]))
+            else:
+              files.append(plotoban(d[0,p,k], x, y, el[0,k].mean(), az[0,k], \
+                           sec_utime.num2date(ti[0,k].mean()), pass_no = p+1, directory = options.dir))
+        
+# Now create the html file for looking at the data
+
+    print
+    print "Creating the HTML file for plots in local directory:  %s" % (options.dir+".html")
+    create_html(options.dir,files)
+    print 
+    
+#-------------------------------------------------------------------------------
+# Main program for testing...
+#
+if __name__ == "__main__":
+    sys.exit(main())
+    
+# End of file
